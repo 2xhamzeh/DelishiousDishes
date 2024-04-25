a671c1ad44b9aff1659786665df21885689341ed
2xhamzeh <hamzehx2@outlook.com>
2024-04-25 16:36:56 +0200

    story_001 viewing user profiles

---
+++ controllers/userController.js
@@ -0,0 +1,7 @@
+exports.sendUserProfile = (req, res) => {
+    
+    // this code is to be replaced once we have user profiles and a data base
+    // instead of sending a text we should send a page that displays the users' profile
+    let id = req.params.id;
+    res.send(`This is the profile for the user with the ID ${id}`);
+}
--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
+const userController = require("./controllers/userController");
 
 app.get("/", (req,res)=>{
     res.send("Welcome to the home Page!");
 });
+
+app.get("/users/:id", userController.sendUserProfile);
 
 app.listen(3000);
