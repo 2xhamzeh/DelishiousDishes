8cf393310e167174f17d3382f2ca885a2bd3cd07
2xhamzeh <hamzehx2@outlook.com>
2024-05-03 14:45:24 +0200

    error handling with custom 404 page

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const userController = require("./controllers/userController");
+const errorController = require("./controllers/errorController");
+
+app.use(express.static('public'));
 
 app.get("/", (req,res)=>{
     res.send("Welcome to the home Page!");
@@ -8,6 +11,11 @@ app.get("/", (req,res)=>{
 
 app.get("/users/:id", userController.sendUserProfile);
 
-app.post("/sign_up", homeController.userSignUpProcessor);
+app.post("/sign_up", userController.userSignUpProcessor);
+
+
+app.use(errorController.logErrors);
+app.use(errorController.respondNoResourceFound);
+app.use(errorController.respondInternalError);
 
 app.listen(3000);
