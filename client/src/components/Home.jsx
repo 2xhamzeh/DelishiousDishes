0d0ce14c92ee0682de480de6ed3644bacc747a47
2xhamzeh <hamzehx2@outlook.com>
2024-06-06 00:10:24 +0200

    added images to home page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -6,22 +6,34 @@ const HomeBody = () => {
     <div>
       <div className="text-center mt-20 mb-10">
         <h1 className="font-bold text-4xl">Welcome to Delishious Dishes!</h1>
-        <h2 className="font-medium text-xl">
+        <h2 className="text-3xl font-light mt-2">
           The perfect place to find and share recipes
         </h2>
       </div>
       <div className="flex flex-auto justify-evenly px-20 font-medium text-lg text-center">
         <Link to="/dishes">
           <h3>Dishes</h3>
-          <img src="/images/placeholder.png" alt="image" />
+          <img
+            src="/images/dish.jpg"
+            alt="image"
+            className=" w-80 h-80 object-cover border"
+          />
         </Link>
         <Link to="/users">
           <h3>People</h3>
-          <img src="/images/placeholder.png" alt="image" />
+          <img
+            src="/images/people.jpg"
+            alt="image"
+            className=" w-80 h-80 object-cover border"
+          />
         </Link>
         <Link to="/help">
           <h3>Help</h3>
-          <img src="/images/placeholder.png" alt="image" />
+          <img
+            src="/images/recipeBook.jpg"
+            alt="image"
+            className=" w-80 h-80 object-cover border"
+          />
         </Link>
       </div>
     </div>
